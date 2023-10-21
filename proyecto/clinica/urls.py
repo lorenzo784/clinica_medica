ce907c71a2db9e33ff8efcf1aca7b4b32f262226
Lorenzo0402 <lopezlorenzo0405@gmail.com>
2023-10-21 00:17:35 -0600

    CRUD completo paciente

--- proyecto/clinica/urls.py
+++ proyecto/clinica/urls.py
@@ -5,5 +5,8 @@ app_name = 'clinica'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('paciente/create', views.PacienteCreateView.as_view(), name='paciente-create')
+    path('paciente/create', views.PacienteCreateView.as_view(), name='paciente-create'),
+    path('paciente', views.PacienteListView.as_view(), name='paciente-list'),
+    path('paciente/editar/<int:pk>', views.PacienteUpdateView.as_view(), name='paciente-update'),
+    path('paciente/eliminar/<int:pk>', views.PacienteDeleteView.as_view(), name='paciente-delete'),
 ]
--- proyecto/clinica/views.py
+++ proyecto/clinica/views.py
@@ -8,8 +8,24 @@ from django.urls import reverse_lazy
 def home(request):
     return render(request, 'clinica/home.html')
 
+
+class PacienteListView(ListView):
+    model = Paciente
+    template_name = 'clinica/paciente/paciente_list.html'
+
 class PacienteCreateView(CreateView):
     model = Paciente
     form_class = PacienteForm
     template_name = 'clinica/paciente/paciente_form.html'
-    success_url = reverse_lazy('clinica:paciente-create')
+    success_url = reverse_lazy('clinica:paciente-list')
+
+class PacienteUpdateView(UpdateView):
+    model = Paciente
+    form_class = PacienteForm
+    template_name = 'clinica/paciente/paciente_form.html'
+    success_url = reverse_lazy('clinica:paciente-list')
+
+class PacienteDeleteView(DeleteView):
+    model = Paciente
+    template_name = 'clinica/paciente/paciente_delete.html'
+    success_url = reverse_lazy('clinica:paciente-list')
