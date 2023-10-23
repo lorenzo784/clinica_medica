520741c55c19ebacf6a8cc4c237bf796b738bdb0
Lorenzo0402 <lopezlorenzo0405@gmail.com>
2023-10-23 13:42:54 -0600

    ham-menu icon position

--- proyecto/staticfiles/js/main.js
+++ proyecto/staticfiles/js/main.js
@@ -2,16 +2,14 @@ const hamMenu = document.querySelector('.ham-menu');
 const navbarModalBackground = document.querySelector('.navbar-modal__background');
 const navbarModalMenu = document.querySelector('.navbar-modal__menu');
 
-hamMenu.addEventListener('click', () =>{
+hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle("active");
     navbarModalBackground.classList.toggle("show");
     navbarModalMenu.classList.toggle("show-menu");
 })
 
-document.addEventListener('click', function (event) {
-    const isClickInsideMenu = hamMenu.contains(event.target) || navbarModalMenu.contains(event.target);
-    if (!isClickInsideMenu) {
-        navbarModalBackground.classList.remove('show');
-        navbarModalMenu.classList.remove("show-menu");
-    }
-});     
+navbarModalBackground.addEventListener('click', () => {
+    hamMenu.classList.toggle("active");
+    navbarModalBackground.classList.remove('show');
+    navbarModalMenu.classList.remove("show-menu");
+});
