efebd69e64e460fd9b4141a8423c7079dc733ce2
Lorenzo0402 <lopezlorenzo0405@gmail.com>
2023-10-26 12:16:32 -0600

    templates and models change

--- proyecto/clinica/models.py
+++ proyecto/clinica/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 class Paciente(models.Model):
     nombre = models.CharField(max_length=128)
-    dpi = models.IntegerField()
+    dpi = models.CharField(max_length=64)
     fecha_de_nacimiento = models.DateField()
     direccion = models.CharField(max_length=128)
     razon_de_visita = models.CharField(max_length=128)
     receta_medica = models.CharField(max_length=128)
-    numero_telefonico = models.IntegerField()
+    numero_telefonico = models.CharField(max_length=64)
 
 
 class Medico(models.Model):
     nombre = models.CharField(max_length=128)
-    numero_colegiado = models.IntegerField()
+    numero_colegiado = models.CharField(max_length=64)
     especialidad = models.CharField(max_length=64)
     diagnostico = models.CharField(max_length=128)
